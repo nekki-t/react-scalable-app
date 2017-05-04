8f7beccfae4091ba916182bd5c12530f2d02483f
nekki-t <nekki.t.0317@gmail.com>
2017-05-04 16:00:38 +0900

    init to styling

---
+++ app/components/Navigation/index.js
@@ -0,0 +1,42 @@
+/**
+ *
+ * Navigation
+ *
+ */
+
+import React from 'react';
+import AppBar from '../AppBar';
+import Drawer from '../Drawer';
+
+import styles from './styles.css';
+
+function Navigation({ topics, selectTopic, toggleDrawer, isDrawerOpen }) {
+  return (
+    <div className={styles.navigation}>
+      <AppBar
+        toggleDrawer={toggleDrawer}
+      />
+      <Drawer
+        items={topics}
+        selectItem={selectTopic}
+        itemLabelAttr="name"
+        itemKeyAttr="name"
+        isDrawerOpen={isDrawerOpen}
+      />
+    </div>
+  );
+}
+
+Navigation.propTypes = {
+  isDrawerOpen: React.PropTypes.bool.isRequired,
+  toggleDrawer: React.PropTypes.func.isRequired,
+  topics: React.PropTypes.arrayOf(
+    React.PropTypes.shape({
+      name: React.PropTypes.string.isRequired,
+      description: React.PropTypes.string.isRequired,
+    })
+  ).isRequired,
+  selectTopic: React.PropTypes.func.isRequired,
+};
+
+export default Navigation;
